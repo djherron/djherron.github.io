b8297c405a3c8fdc0cf686b63fa4122ec5d25cf5
djherron <dave.herron1@gmail.com>
2025-05-08 04:51:20 +0000

    Deploying to gh-pages from @ djherron/djherron.github.io@7e0b6c921cfea0e0ee347091bc0cc55e5e4cd683 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -482,35 +482,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%61%76%69%64.%68%65%72%72%6F%6E@%63%69%74%79%73%74%67%65%6F%72%67%65%73.%61%63.%75%6B", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/djherron", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/david-herron-7054131aa", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0009-0008-2736-6789", "_blank");
         },
       },{
       id: 'light-theme',
